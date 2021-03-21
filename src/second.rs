98dbd461a5e45923d905d1deda1aca5031a04f93
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2021-03-21 18:41:08 +0530

    using language features

---
+++ src/second.rs
@@ -0,0 +1,67 @@
+pub struct List {
+    head: Link
+}
+
+type Link = Option<Box<Node>>;
+
+struct Node {
+    value: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, value: i32) {
+        let new_node = Box::new(Node {
+            value,
+            next: self.head.take(),
+        });
+        self.head = Some(new_node)
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.value
+        })
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_node = self.head.take();
+        while let Some(mut boxed_nodes) = cur_node {
+            cur_node = boxed_nodes.next.take();
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+
+        assert_eq!(list.pop(), None);
+    }
+}
